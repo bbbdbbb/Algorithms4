89ffc468692b6b4a32be420fc293d89ea4852268
bbbdbbb <1369603142@qq.com>
2020-01-03 23:49:18 +0800

    create a test-class to test temprary codes

---
+++ Algorithms4/src/Test/HelloWorld.java
@@ -0,0 +1,19 @@
+package Test;
+
+/**   
+*    
+* 项目名称：Algorithms4   
+* 类名称：HelloWorld   
+* 类描述：该类用于临时测试代码
+* 创建人：bbbdbbb   
+* 创建时间：2020年1月3日 下午11:45:50   
+* @version        
+*/
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!");
+
+	}
+
+}

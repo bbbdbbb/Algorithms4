8f47f9cdb20691d6f8730c54f1b2bd4f89d530b9
bbbdbbb <1369603142@qq.com>
2020-01-06 23:57:30 +0800

    creater P1_1_01 class

---
+++ Algorithms4/src/chapter1/P1_1_01.java
@@ -0,0 +1,18 @@
+package chapter1;
+
+/**   
+*    
+* 项目名称：Algorithms4   
+* 类名称：P1_1_1   
+* 类描述：第一章，习题1.1.01   
+* 创建人：Administrator   
+* 创建时间：2020年1月6日 下午11:48:56   
+* @version        
+*/
+public class P1_1_01 {
+
+	public static void main(String[] args) {
+
+	}
+
+}

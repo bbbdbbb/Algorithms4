cf92fdbf30709e70edce760a59cd782ff518154a
bbbdbbb <1369603142@qq.com>
2020-01-09 23:14:52 +0800

    create P1_1_02

--- Algorithms4/src/chapter1/P1_1_01.java
+++ Algorithms4/src/chapter1/P1_1_01.java
@@ -5,7 +5,7 @@ package chapter1;
 * 项目名称：Algorithms4   
 * 类名称：P1_1_1   
 * 类描述：第一章，习题1.1.01   
-* 创建人：Administrator   
+* 创建人：bbbdbbb   
 * 创建时间：2020年1月6日 下午11:48:56   
 * @version        
 */
---
+++ Algorithms4/src/chapter1/P1_1_02.java
@@ -0,0 +1,28 @@
+package chapter1;
+
+/**   
+*    
+* 项目名称：Algorithms4   
+* 类名称：P1_1_02   
+* 类描述：第一章，习题1.1.02   
+* 创建人：bbbdbbb   
+* 创建时间：2020年1月9日 下午11:09:25   
+* @version        
+*/
+public class P1_1_02 {
+
+	public static void main(String[] args) {
+		//a.(1+2.236)/2
+		System.out.println((1+2.236)/2);//	1.618
+		
+		//b. 1+2+3+4.0
+		System.out.println(1+2+3+4.0);//	10.0
+		
+		//c. 4.1>=4
+		System.out.println(4.1>=4);//	true
+		
+		//d.1+2+"3"
+		System.out.println(1+2+"3");//	33
+	}
+
+}

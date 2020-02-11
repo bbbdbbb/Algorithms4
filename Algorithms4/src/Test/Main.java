16cdbd36d0e7f61ed95134c3737099ac5e2aa35b
bbbdbbb <1369603142@qq.com>
2020-02-11 22:24:11 +0800

    oj test

--- Algorithms4/src/Test/Main.java
+++ Algorithms4/src/Test/Main.java
@@ -4,30 +4,31 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
+	public static int update(int ans,int a,int b){
+		if(ans<(a^b))		return a^b;
+		else				return ans;
+	}
 
 	public static void main(String[] args) {
 		Stack<Integer> stack = new Stack<Integer>();
-		String end = "";
-		int N=0;
-		Scanner scan = new Scanner(System.in);
-		if(scan.hasNext())	N = scan.nextInt();
-		
-		int last = 1;
-		int cur=0;
-		for(int i=1;i<=N;i++){
-			if(scan.hasNext())	cur = scan.nextInt();
-			for(;last<=cur;last++){
-				stack.push(i);
-			}
-			int pos = stack.pop();
-			if(i==N){
-				end += (i-pos+1)+"";
-			}else{
-				end += (i-pos+1)+" ";
-			}
-		}
-		System.out.println(end);
+		int ans = 0;
 
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int a[] = new int[N];
+		
+		for(int i=0;i<N;i++){
+			int t = scan.nextInt();
+			while(!stack.isEmpty() && stack.peek()<t){
+				ans = update(ans,stack.peek(),t);
+				stack.pop();
+			}
+			if(!stack.isEmpty())
+				ans = update(ans,stack.peek(),t);
+			stack.push(t);
+		}
+		
+		System.out.println(ans);
 	}
 
 }

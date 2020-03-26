1f92300b4dc7dba699db7f1648502cee0712204a
bbbdbbb <1369603142@qq.com>
2020-03-26 21:47:18 +0800

    add algo15

--- OJ/src/greedy/ALGO_15.java
+++ OJ/src/greedy/ALGO_15.java
@@ -1,12 +1,22 @@
-package test;
+package greedy;
 
-import java.util.Comparator;
 import java.util.Scanner;
 
-public class Main {
+/** 贪心算法
+ * 算法训练 旅行家的预算 
+ * http://lx.lanqiao.cn/problem.page?gpid=T75 
+*    
+* 项目名称：OJ   
+* 类名称：ALGO_15   
+* 类描述：   
+* 创建人：bbbdbbb   
+* 创建时间：2020年3月26日 下午9:45:07   
+* @version        
+*/
+public class ALGO_15 {
 
 	public static void main(String[] args) {
-		long startMili=System.currentTimeMillis();
+
 
 		Scanner scan = new Scanner(System.in);
 		double D1 = scan.nextDouble();
@@ -51,11 +61,11 @@ public class Main {
 					System.out.println("1打桩" + i +" : 不用加油     j = " + j + "   res = " + res +"     ds[j] = " + ds[j] + "   ds[i]"+ds[i]);
 				}else{//需要加适量的油才可以到达第j个加油站
 					double s = ds[j] - ds[i];
-					double tem = res * D2;
-					s = s - tem; //s - 油箱剩余的油能行驶的路程
-					ans += (s / D2) * ps[i];
+					double tem = (double)Math.round((res * D2)*1000)/1000;
+					s = (double)Math.round((s - tem) * 1000) / 1000; //s - 油箱剩余的油能行驶的路程
+					ans = (double)Math.round(( ans + (double)(Math.round((s / D2) * ps[i] * 1000)) /1000) *1000) / 1000;
 					//不要忘记加油
-					res += s / D2;
+					res = (double)Math.round((res + (double)(Math.round((s / D2) * 1000)) / 1000) * 1000) / 1000;
 					
 					
 					System.out.println("2打桩" + i +" : ans = " + ans + "   ps[i] = " + ps[i] + "    s = "+s  + "  res = "+res);
@@ -63,7 +73,7 @@ public class Main {
 				}
 			}else{//在第i个加油站 加满C升油 不能 到达第j个加油站
 				//把油加满
-				ans += (C - res) * ps[i];
+				ans = (double)Math.round((ans +(C - res) * ps[i]) * 1000) / 1000;
 				res = C;
 				System.out.println("3打桩" + i +" : ans = " + ans + "   ps[i] = " + ps[i]);
 			}
@@ -73,43 +83,46 @@ public class Main {
 			if(j + 1 == i){
 				res = 0;
 			}else{
-				res -= (ds[i+1] - ds[i]) / D2;
+				res = (double)Math.round((res - (double)(Math.round((ds[i+1] - ds[i]) / D2 * 1000)) / 1000) * 1000) /1000;
 			}
 		}
 		
 			
 		
-
+//第一次做题时，错误的思路：每次只比较 第i个加油站 和第i+1个加油站 之间的油价关系：
+//		for(; i <= N; i++){
+//			if(C * D2 < (ds[i+1] - ds[i])){
+//				//在第i站，把油箱加满都不能到达下一站
+//				break;
+//			}
+//			
+//			if(ps[i] < ps[i+1]){//当前第i个加油站的油价 低于 下一个 i+1油站
+//				//把邮箱加满
+//				ans += (C - res) * ps[i];
+//				System.out.print("打桩"+i+": "+ans);
+//				
+//				res = C - (ds[i+1] - ds[i]) / D2;
+//				System.out.println("  res = "+res);
+//			}else{//当前第i个加油站的油价 高于于 下一个 i+1油站
+//				//油加到够用就行
+//				double s = ds[i+1] - ds[i];
+//				s = s - res * D2;//s - 油箱剩余的油能行驶的路程
+//				ans += (s / D2) * ps[i];
+//				System.out.println("打桩"+i+": "+ans);
+//
+//				res = 0;
+//			}
+//			
+//			//模拟汽车从加油站i 行驶到 加油站i+1 消耗油的过程
+//			//res = res - (ds[i+1] - ds[i]) / D2;  //代码优化： 将模拟过程直接添加到加油的时候，减少运算
+//		}
+		
 		if(i == N+1){//成功到达终点
 			System.out.printf("%.2f",ans);
 		}else{//中间break 了
 			System.out.println("No Solution");
 		}
 		
-		System.out.printf("%.2f",0.156);
-		
-		//计时，在提交答案的时候要删除
-		//long endMili=System.currentTimeMillis();
-		//System.out.println("总耗时为："+(endMili-startMili)+"毫秒");
 	}
-	
-	public class node{
-		public int k;
-		public int a1;
-		public int x;
-		public int y;
-		public int m;
-	}
-	
-	//创建node的比较器
-	public class nodeComparator implements Comparator<node>{
 
-		@Override
-		public int compare(node o1, node o2) {
-			return o1.a1 - o2.a1;
-		}
-		
-	}
-	
-	
 }
--- OJ/src/greedy/ALGO_34.java
+++ OJ/src/greedy/ALGO_34.java
@@ -3,6 +3,21 @@ package greedy;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/** 贪心算法，细节。虽然自己凑出了所以情况，但是感觉代码不够优雅，过段时间优化一下
+ * 算法训练 纪念品分组
+ * http://lx.lanqiao.cn/problem.page?gpid=T96
+ * 
+ * 问题描述
+*　元旦快到了，校学生会让乐乐负责新年晚会的纪念品发放工作。为使得参加晚会的同学所获得的纪念品价值 相对均衡，他要把购来的纪念品根据价格进行分组，但每组最多只能包括两件纪念品，并且每组纪念品的价格之和不能超过一个给定的整数。为了保证在尽量短的时 间内发完所有纪念品，乐乐希望分组的数目最少。
+*　你的任务是写一个程序，找出所有分组方案中分组数最少的一种，输出最少的分组数目。  
+*    
+* 项目名称：OJ   
+* 类名称：ALGO_34   
+* 类描述：   
+* 创建人：bbbdbbb   
+* 创建时间：2020年3月25日 下午10:41:43   
+* @version        
+*/
 public class ALGO_34 {
 
 	public static int[] ns;
@@ -27,14 +42,14 @@ public class ALGO_34 {
 			boolean flag = false;
 			int j = 0;
 			for(; j < i; j++){
-				if(ns[j] + ns[i] == w){
+				if(ns[j] + ns[i] == w && !bis[j]){
 					//找到两个元素 可以 分成一组
 					bis[j] = true;
 					bis[i] = true;
-					System.out.println("打桩："+ns[i]+" and "+ns[j]);
+					//System.out.println("打桩："+ns[i]+" and "+ns[j]);
 					ans++;
 					break;
-				}else if(ns[j] + ns[i] > w || j == i - 1){
+				}else if(ns[j] + ns[i] > w && !bis[j]){
 					
 					while(--j >= 0){
 						if(bis[j]){
@@ -43,7 +58,7 @@ public class ALGO_34 {
 							//a[j]和a[i]分成一组
 							bis[j] = true;
 							bis[i] = true;
-							System.out.println("打桩："+ns[i]+" and "+ns[j]);
+							//System.out.println("打桩："+ns[i]+" and "+ns[j]);
 							flag = true;
 							break;
 						}
@@ -54,17 +69,28 @@ public class ALGO_34 {
 						break;
 					}else{//a[i]不能和其他纪念品组合，只能单独分组
 						bis[i] = true;
-						System.out.println("打桩："+ns[i]);
+						//System.out.println("打桩："+ns[i]);
 						break;
 					}
 					
 				}
 			}
 			
+
 			if(j == i){
-				ans++;
 				bis[i] = true;
-				System.out.println("打桩："+ns[i]);
+				//System.out.print("打桩："+ns[i]);
+				ans++;
+				for(--j; j >= 0; j--){
+					if(bis[j]){
+						continue;
+					}else{
+						bis[j] = true;
+						//System.out.print(" and "+ns[j]);
+						break;
+					}
+				}
+				//System.out.println();
 			}
 			
 		}
